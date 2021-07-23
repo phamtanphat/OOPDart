89e6ee57b97b2ad2e7232706a5b3929864261185
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-23 19:29:01 +0700

    oop getter setter

--- animal.dart
+++ animal.dart
@@ -1,4 +1,25 @@
 class Animal{
   String category;
   int weight;
+  num _height;
+
+  // alt + insert
+
+  num get height => _height;
+
+  set height(num value) {
+    _height = value;
+  }
+
+// getter setter
+
+  // num get height => _height;
+  //
+  // void set height(num height){
+  //   _height = height;
+  // }
+
+
+
+
 }
--- main.dart
+++ main.dart
@@ -5,4 +5,8 @@ void main(){
   var cat = Animal();
   cat.category = 'Cat';
   cat.weight = 2;
+
+  cat.height = 0.5;
+
+  print(cat.height);
 }
