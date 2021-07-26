16d9bc61fcfb40142bd481c548ccfbca703da8b4
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-26 18:23:49 +0700

    positional option parameter

--- main.dart
+++ main.dart
@@ -5,9 +5,19 @@ void main(){
   // báo lỗi trong quá trình runtime
 
   // ctrl + P
-  var cat = Animal.constructor2("Cat",2);
+  // var cat = Animal.constructor2("Cat",2);
+  // print(cat);
+  showCountry("Viet nam",false);
+}
 
-  print(cat);
-
-
+void showCountry(String country,[bool? upperCase]){
+  if (upperCase != null){
+    if(upperCase){
+      print(country.toUpperCase());
+    }else{
+      print(country);
+    }
+  }else{
+    print(country);
+  }
 }
