3f2af711792d333a3bb38156af32159792fe5a36
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-26 18:35:53 +0700

    optional name parameter

--- main.dart
+++ main.dart
@@ -1,32 +1,19 @@
 import 'animal.dart';
 
-void main(){
-
+void main() {
   // báo lỗi trong quá trình runtime
 
   // ctrl + P
   // var cat = Animal.constructor2("Cat",2);
   // print(cat);
-  showCountry("Viet nam",false);
+  showCountry(country2: "Mỹ" );
 }
 
-void showCountry(String country,[bool? upperCase]){
-  if (upperCase != null){
-    if(upperCase){
-      print(country.toUpperCase());
-    }else{
-      print(country);
-    }
-  }else{
+void showCountry({String? country, String? country2}) {
+  if (country != null) {
     print(country);
   }
-}
-
-// default parameter : Giá trị mặc định
-void showCountry(String country,[bool upperCase = false]){
-  if(upperCase){
-    print(country.toUpperCase());
-  }else{
-    print(country);
+  if (country2 != null) {
+    print(country2);
   }
 }
